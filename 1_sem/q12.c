aff560d01bb1ec045c60d6c64bb991c518db427e
anil-650 <anilhansda65@gmail.com>
2021-10-16 16:15:45 +0530

    added q13 unfinished q12 opt e remaining

--- 1_sem/q12.c
+++ 1_sem/q12.c
@@ -1,13 +1,13 @@
 /*
-*12. Write a Program to perform following actions on an array entered by the user:
-*
-*a)Print the even-valued elements
-*b)Print the odd-valued elements
-*c)Calculate and print the sum and average of the elements of array
-*d)Print the maximum and minimum element of array
-*e)Remove the duplicates from the array
-*f)Print the array in reverse order
-*/
+ *12. Write a Program to perform following actions on an array entered by the user:
+ *
+ *a)Print the even-valued elements
+ *b)Print the odd-valued elements
+ *c)Calculate and print the sum and average of the elements of array
+ *d)Print the maximum and minimum element of array
+ *e)Remove the duplicates from the array
+ *f)Print the array in reverse order
+ */
 
 
 #include <stdio.h>
@@ -19,18 +19,25 @@ void load_array(int s[])
     printf("Enter the no of elements: ");
     scanf("%d",&no);
 
-    for(i = 1; i <= no; i++)
+    for(i = 0; i < no; i++)
     {
-        printf("element [%d] = ",i);
+        printf("element [%d] = ",i+1);
         scanf("%d",&s[i]);
     }
+
+    printf("\nArray:\n");
+
+    for(i = 0; i < no ; i++)
+        printf("%d ",s[i]);
+    printf("\n");
+
 }
 
-void a(int h[])
+void a(int h[])     //even elements of array
 {
-    printf("Even elements in array : ");
+    printf("\nEven elements in array : ");
 
-    for(i = 1; i <= no; i++)
+    for(i = 0; i < no; i++)
     {
         if(h[i]%2 == 0)
             printf("%d ",h[i]);
@@ -39,11 +46,11 @@ void a(int h[])
     printf("\n");
 }
 
-void b(int k[])
+void b(int k[])     //odd elements of array
 {
-    printf("Odd elements in array : ");
+    printf("\nOdd elements in array : ");
 
-    for(i = 1; i <= no; i++)
+    for(i = 0; i < no; i++)
     {
         if(k[i]%2 != 0)
             printf("%d ",k[i]);
@@ -52,11 +59,11 @@ void b(int k[])
     printf("\n");
 }
 
-void c(int j[])
+void c(int j[])     //sum & avg of array
 {
     int sum=0, avg=0;
 
-    for(i = 1; i <= no; i++)
+    for(i = 0; i < no; i++)
         sum+=j[i];
 
     avg = sum/no;
@@ -65,26 +72,111 @@ void c(int j[])
             "\nAverage of elements: %d\n ",sum,avg);
 }
 
-void d(int l[])
+void d(int l[])     //min max element of array
 {
-    printf("Odd elements in array : ");
+    int large,small;
+    large = small = l[0];
 
-    for(i = 1; i <= no; i++)
+    for(i = 0; i < no; i++)
     {
-        // print max & min element of array
+        if(l[i] > large)
+            large = l[i];
+
+        if(l[i] < small)
+            small = l[i];
     }
+
+    printf("\nMax element:%d\nMin element:%d\n",large,small);
+}
+
+void e(int m[])     //delete duplicate from array NOT WORKING
+{
+    int uniq,exsist,j=0,k,temp[]={0};
+
+    for(i = 0; i < no; i++)
+    {
+        uniq=m[i];
+        exsist=0;
+        for(k = 0; k < i; k++){
+            if(temp[k]==uniq)
+                exsist=1;
+        }
+
+        if(!exsist){
+            temp[j]=m[i];
+            j++;
+        }
+
+        printf("\nAfter deleting duplicates from Array:\n");
+        for(i = 0; i < no ; i++)
+            printf("%d ",temp[i]);
+
+
+    }
+}
+
+void f(int o[])     //print array in reverse
+{
+    printf("\nArray in Reverse:\n");
+
+    for(i = no-1; i >= 0; i--)
+        printf("%d ",o[i]);
 
     printf("\n");
 }
 
+void opt_show()
+{
+    printf("\n"
+            "a)Print the even-valued elements\n"
+            "b)Print the odd-valued elements\n"
+            "c)Calculate and print the sum and average of the elements of array\n"
+            "d)Print the maximum and minimum element of array\n"
+            "e)Remove the duplicates from the array\n"
+            "f)Print the array in reverse order\n"
+            "\n");
+}
 
 int main (){
     int arr[]={0};
+    char x;
 
     load_array(arr);
-    a(arr);
-    b(arr);
-    c(arr);
+
+    opt_show();
+
+    printf("\nEnter option: ");
+    scanf(" %c",&x);
+
+    switch(x){
+defult:
+        printf("\nInvalid\n");
+        break;
+
+        case 'a':
+        a(arr);
+        break;
+
+        case 'b':
+        b(arr);
+        break;
+
+        case 'c':
+        c(arr);
+        break;
+
+        case 'd':
+        d(arr);
+        break;
+
+        case 'e':
+        e(arr);
+        break;
+
+        case 'f':
+        f(arr);
+        break;
+    }
 
     return 0;
 }
---
+++ 1_sem/q13.c
@@ -0,0 +1,10 @@
+//13. Write a Program that prints a table indicating the number of occurrences of each alphabet in the text entered as command line arguments.
+
+
+
+#include <stdio.h>
+
+int main (){
+
+    return 0;
+}
