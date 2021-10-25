1026a679095870b14533487a59f044e407a2d341
anil-650 <anilhansda65>
2021-10-25 11:53:36 +0530

    complete q20

--- 1_sem/q20.c
+++ 1_sem/q20.c
@@ -7,21 +7,31 @@ int main()
 {
     char ch;// source_file[20], target_file[20];
     FILE *source, *target;
-    char source_file[]="x1.txt";
-    char target_file[]="x2.txt";
-    source = fopen(source_file, "r");
+    char f1[20];
+    char f2[20];
+
+    printf("Enter file name to copy from :\n");
+    scanf("%s",f1);
+
+    source = fopen(f1, "r");
     if (source == NULL) {
-        printf("Press any key to exit...\n");
+        printf("File not found\n");
         exit(EXIT_FAILURE);
     }
-    target = fopen(target_file, "w");
+
+    printf("Enter file name to copy from :\n");
+    scanf("%s",f2);
+
+    target = fopen(f2, "w");
     if (target == NULL) {
         fclose(source);
         printf("Press any key to exit...\n");
         exit(EXIT_FAILURE);
     }
+
     while ((ch = fgetc(source)) != EOF)
         fputc(ch, target);
+    
     printf("File copied successfully.\n");
     fclose(source);
     fclose(target);
