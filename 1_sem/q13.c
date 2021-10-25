7ce6ee0497a274e51f16ea902bace11045a189c0
anil-650 <anilhansda65@gmail.com>
2021-10-25 06:57:40 +0530

    added q13.c

--- 1_sem/q13.c
+++ 1_sem/q13.c
@@ -3,8 +3,32 @@
 
 
 #include <stdio.h>
+#include <string.h>
 
 int main (){
+    char s[30];
+    int i,j,n,count;
 
-    return 0;
-}
+    printf("Enter string:\n");
+    scanf("%s",s);
+    n =strlen(s);
+
+    for(i=0;i<n;i++)  
+    {
+        count=1;
+        if(s[i])
+        {
+            for(j=i+1;j<n;j++)  
+            {   
+                if(s[i]==s[j])
+                {
+                    count++;
+                    s[j]='\0';
+                }
+            }  
+            printf("'%c' = %d \n",s[i],count);
+        }
+    }
+
+        return 0;
+    }
