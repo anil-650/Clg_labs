0c5d326d38c2f2e19e1ae8de98f23df302f0d6ca
anil-650 <anilhansda65@gmail.com>
2021-10-26 19:52:12 +0530

    added q15

---
+++ 1_sem/q15.c
@@ -0,0 +1,29 @@
+//15. Write a program in which a function is passed address of two variables and then alter its contents.
+
+#include <stdio.h>
+
+void alt(int a, int b){
+    int temp;
+
+    printf("Before altering num1 = %d\tnum2 = %d\n",a,b);
+
+    temp = a;
+    a = b;
+    b = temp;
+
+    printf("After altering  num1 = %d\tnum2 = %d\n",a,b);
+}
+
+int main (){
+    int num1, num2;
+
+    printf("Enter 2 no.\n");
+    printf("Enter num1: ");
+    scanf("%d",&num1);
+    printf("Enter num2: ");
+    scanf("%d",&num2);
+
+    alt(num1,num2);
+
+    return 0;
+}
