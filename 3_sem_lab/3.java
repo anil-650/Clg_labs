1fd979218a03bbf7b79b881e1516e89a7467277f
anil65 <anilhansda65@gmail.com>
2022-05-12 16:14:38 +0530

    added 3.java

---
+++ 3_sem_lab/3.java
@@ -0,0 +1,7 @@
+//3. To convert a decimal to binary number.
+class decimal_to_binary{
+	public static void main(String[] args) {
+		System.out.println(Integer.toBinaryString(10));
+		System.out.println(Integer.toBinaryString(21));
+	}
+}
