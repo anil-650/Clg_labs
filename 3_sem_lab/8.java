77f5b159dd1ca754df57b6735f418c0b2354e621
anil-650 <anilhansda65@gmail.com>
2022-07-30 20:45:33 +0530

    added 7 8 java

---
+++ 3_sem_lab/8.java
@@ -0,0 +1,3 @@
+//8. Modify the distance" class by creating constructor for assigning values. (feetandinches) to the distance object. Create another object and assign second object as reference variable to another object reference variable. Further create a third object which is a clone of the first object.
+
+
