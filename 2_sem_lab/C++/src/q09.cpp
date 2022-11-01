1648e8242003b6965266c09263f21ae2218df6b6
anil-650 <anilhansda65@gmail.com>
2022-11-01 15:18:28 +0530

    added q 7-9

--- 2_sem_lab/C++/src/q09.cpp
+++ 2_sem_lab/C++/src/q09.cpp
@@ -1,1 +1,24 @@
-9. Write a program to find square and cube of a number using inline function.
+// 9. Write a program to find square and cube of a number using inline function.
+
+#include <iostream>
+
+using namespace std;
+
+inline int square(int a){
+    return a*a;
+}
+
+inline int cube(int a){
+    return a*a*a;
+}
+
+int main(){
+    int num;
+    cout << "Enter a number: " ;
+    cin >> num;
+
+    cout << "Square of number: " << square(num) << endl;
+    cout << "Cube of number: " << cube(num) << endl;
+
+    return 0;
+}
