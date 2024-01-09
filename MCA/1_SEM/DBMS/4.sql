7cd490e0b432a627e4ae60d7fcc4ee587229ebb7
anil65 <anilhansda65@gmail.com>
2024-01-09 20:14:41 +0530

    minor fixes

--- MCA/1_SEM/DBMS/4.sql
+++ MCA/1_SEM/DBMS/4.sql
@@ -4,7 +4,7 @@ CREATE TABLE Product(
 	current Data NUMBER(32)
 );
 
--- Lets create  a before and after  statement and row lwvwl trriggers for the Product table
+-- Lets create  a before and after  statement and row level trriggers for the Product table
 -- A/ Before update, statement level
 
 CREATE OR REPLACE TRIGGER BEFORE update_state Product
@@ -12,8 +12,8 @@ BEFORE
 UPDATE ON Product
 BEGIN
 	INSERT INTO Product_cheer VALUES(
-		'Before update, statement level: Syntax'
-	):
+		'Before update, statement level; Syntax'
+	);
 END;
 
 -- B/ Before update, row level
@@ -33,8 +33,8 @@ AFTER
 UPDATE ON Product
 BEGIN
 	INSERT INTO Product_cheer VALUES(
-		'Before update, statement level: Syntax'
-	):
+		'After update, statement level: Syntax'
+	);
 END;
 
 -- D/ After update, row level
@@ -47,9 +47,9 @@ FOR EACH ROW
 END;
 
 -- Now Lets execute a update statement on table Product
-UPDATE Product SET unit price 800 WHERE Product_ID in (100,101)
+UPDATE Product SET unit price 800 WHERE Product_ID in (100,101);
 
--- Lets check the dat in Product_cheer table to see the order in which the
+-- Lets check the data in Product_cheer table to see the order in which the
 -- trigger is tried
 SELECT * FROM Product_cheer;
 
